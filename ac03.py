02e4c357b89497284cf5c87934ea754036622928
Guilherme-Castilhone <54124601+Guilherme-Castilhone@users.noreply.github.com>
2020-04-20 21:20:56 -0300

    Update ac03.py

--- ac03.py
+++ ac03.py
@@ -4,6 +4,8 @@ conn = sqlite3.connect('example.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE ac03 (id int primary key, nome text not null, email)''')
 
+c.execute("INSERT INTO ac03 ('1', '', '')")
+
 conn.commit()
 conn.close()
           
